dd0e7064978b135662e0be333f0994d9d9eda13a
Yongveloper <aaa9015@naver.com>
2021-08-29 19:42:06 +0900

    삼각형 판별하기 기본 if문 복습 + 최대값 구하고 삼각형 판별하기

---
+++ 섹션1 기본 문제 풀이/2. 삼각형 판별하기/index.js
@@ -0,0 +1,15 @@
+function solution(a, b, c) {
+  let answer = '';
+  const max = Math.max(a, b, c);
+  const sum = a + b + c;
+
+  if (sum - max < max) {
+    answer = 'No';
+  } else {
+    answer = 'Yes';
+  }
+
+  return answer;
+}
+
+console.log(solution(13, 33, 17));
