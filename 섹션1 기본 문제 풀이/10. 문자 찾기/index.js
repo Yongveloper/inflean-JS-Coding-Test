a479c8165e22ea781b5015aa225a7ff046c6bd77
yongveloper <aaa9015@naver.com>
2021-08-31 16:46:41 +0900

    10. 문자 찾기 split 활용 함수에서 변수 활용

--- 섹션1 기본 문제 풀이/10. 문자 찾기/index.js
+++ 섹션1 기본 문제 풀이/10. 문자 찾기/index.js
@@ -14,7 +14,7 @@ let str = 'COMPUTERPROGRAMMING';
 console.log(solution(str, 'R'));
 
 function solution2(s, t) {
-  let answer = s.split('R').length - 1;
+  let answer = s.split(t).length - 1;
 
   return answer;
 }
