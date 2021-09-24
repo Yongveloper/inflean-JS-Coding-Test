453c137a8893da751fb0378ee75d949b649e1af4
yongveloper <aaa9015@naver.com>
2021-09-24 11:21:39 +0900

    4-5 k번째 큰수 Set 객체를 사용한 풀이 (Set은 중복값을 갖지 않음)

--- 섹션4 완전탐색/5. k번째 큰 수/index.js
+++ 섹션4 완전탐색/5. k번째 큰 수/index.js
@@ -1,19 +1,17 @@
 function solution(n, k, card) {
   let answer = 0;
-
-  const temp = [];
+  const temp = new Set();
 
   for (let i = 0; i < n; i++) {
     for (let j = i + 1; j < n; j++) {
-      for (let s = j + 1; s < n; s++) {
-        temp.push(card[i] + card[j] + card[s]);
+      for (let k = j + 1; k < n; k++) {
+        temp.add(card[i] + card[j] + card[k]);
       }
     }
   }
 
-  temp.sort((a, b) => b - a);
-  console.log(temp);
-  answer = temp[k - 1];
+  const a = Array.from(temp).sort((a, b) => b - a);
+  answer = a[k - 1];
 
   return answer;
 }
