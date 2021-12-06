63f31eff3f61f1b2176478fa3cdcc21d225a122f
yongveloper <aaa9015@naver.com>
2021-12-06 11:06:36 +0900

    7-12 마구간 정하기 결정 알고리즘 응용

---
+++ 섹션7 정렬,그리디,결정/12. 마구간/index.js
@@ -0,0 +1,35 @@
+function count(stable, dist) {
+  let count = 1;
+  let endPos = stable[0];
+
+  for (let i = 1; i < stable.length; i++) {
+    if (stable[i] - endPos >= dist) {
+      count++;
+      endPos = stable[i];
+    }
+  }
+
+  return count;
+}
+
+function solution(c, stable) {
+  let answer;
+  stable.sort((a, b) => a - b);
+  let lt = 1;
+  let rt = stable[stable.length - 1];
+
+  while (lt <= rt) {
+    const mid = Math.floor((lt + rt) / 2);
+    if (count(stable, mid) >= c) {
+      answer = mid;
+      lt = mid + 1;
+    } else {
+      rt = mid - 1;
+    }
+  }
+
+  return answer;
+}
+
+let arr = [1, 2, 8, 4, 9];
+console.log(solution(3, arr));
