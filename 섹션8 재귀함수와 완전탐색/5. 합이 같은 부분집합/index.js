4faab94b56d7e7f32d1b2bfc1f481188bbb177da
yongveloper <aaa9015@naver.com>
2021-12-08 14:21:03 +0900

    8-5 합이 같은 부분집합 DFS 집합 문제 응용

---
+++ 섹션8 재귀함수와 완전탐색/5. 합이 같은 부분집합/index.js
@@ -0,0 +1,24 @@
+function solution(arr) {
+  let answer = 'NO';
+  let total = arr.reduce((acc, cur) => acc + cur);
+  let n = arr.length;
+  let flag = 0;
+
+  function DFS(L, sum) {
+    if (flag) return;
+    if (L === n) {
+      if (total - sum === sum) {
+        answer = 'YES';
+        flag = 1;
+      }
+    } else {
+      DFS(L + 1, sum + arr[L]);
+      DFS(L + 1, sum);
+    }
+  }
+  DFS(0, 0);
+  return answer;
+}
+
+let arr = [1, 3, 5, 6, 7, 10];
+console.log(solution(arr));
