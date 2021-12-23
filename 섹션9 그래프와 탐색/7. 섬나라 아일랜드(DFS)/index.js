6508c25b2911f90078af99c3458ca1b0237df7fd
yongveloper <aaa9015@naver.com>
2021-12-23 10:17:15 +0900

    9-7 섬나라 아일랜드(DFS)

---
+++ 섹션9 그래프와 탐색/7. 섬나라 아일랜드(DFS)/index.js
@@ -0,0 +1,41 @@
+function solution(board) {
+  let answer = 0;
+  const n = board.length;
+  const dx = [-1, -1, 0, 1, 1, 1, 0, -1];
+  const dy = [0, 1, 1, 1, 0, -1, -1, -1];
+
+  function dfs(x, y) {
+    board[x][y] = 0;
+    for (let k = 0; k < 8; k++) {
+      const nx = x + dx[k];
+      const ny = y + dy[k];
+      if (nx >= 0 && nx < n && ny >= 0 && ny < n && board[nx][ny] === 1) {
+        dfs(nx, ny);
+      }
+    }
+  }
+
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n; j++) {
+      if (board[i][j] === 1) {
+        console.log(i, j);
+        answer++;
+        dfs(i, j);
+      }
+    }
+  }
+
+  return answer;
+}
+
+let arr = [
+  [1, 1, 0, 0, 0, 1, 0],
+  [0, 1, 1, 0, 1, 1, 0],
+  [0, 1, 0, 0, 0, 0, 0],
+  [0, 0, 0, 1, 0, 1, 1],
+  [1, 1, 0, 1, 1, 0, 0],
+  [1, 0, 0, 0, 1, 0, 0],
+  [1, 0, 1, 0, 1, 0, 0],
+];
+
+console.log(solution(arr));
